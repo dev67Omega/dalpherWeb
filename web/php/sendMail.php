01f34bd63d06e3ef0677f4997afea34e3ac39485
dev67Omega <omegared@iMac-de-omegaRed.local>
2022-03-15 10:20:26 -0600

    se agrega funcionalidad para formulario

---
+++ web/php/sendMail.php
@@ -0,0 +1,16 @@
+<?php
+
+$user = json_decode(file_get_contents('php://input'));
+
+$namePeople = $user->nameUserCntact;
+$mailPeople = $user->mailUserContact;
+$phonePeople = $user->telUserContact;
+$msgPeople = $user->bodyMsgContact;
+
+$to = "ventas@dalpher.com";
+$subject = "Formulario contacto";
+$txt = " Correo de: ".$namePeople."\n"." Teléfono: ".$phonePeople."\n"." Descripción del mensaje: ".$msgPeople;
+$headers  = "From: ".$mailPeople;
+
+mail($to,$subject,$txt,$headers);
+?> 
